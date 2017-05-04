87210f2617230d48b65c05044148f5af591be7b5
costyn <costynvd@gmail.com>
2017-05-04 11:51:59 -0400

    Rewrite using TaskScheduler
    Rewritten with TaskScheduler in preparation for MPU6050 code
    integration. Further code cleanup, comments, less global variables.

--- glow-fur-pro-mini.ino
+++ glow-fur-pro-mini.ino
@@ -9,120 +9,144 @@
    - choose 1 color, brightenall to max, then fade to min
    - heartbeat pulse
    - color rain https://www.youtube.com/watch?v=nHBImYTDZ9I
+   - two "faders" moving back and forth
 */
 
-
 #include <FastLED.h>
+#include <TaskScheduler.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_PRINT(x)       Serial.print (x)
+#define DEBUG_PRINTDEC(x)    Serial.print (x, DEC)
+#define DEBUG_PRINTLN(x)     Serial.println (x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTDEC(x)
+#define DEBUG_PRINTLN(x)
+#endif
 
 #define LED_PIN     12   // which pin your Neopixels are connected to
 #define NUM_LEDS    89   // how many LEDs you have
-#define BRIGHTNESS 255  // 0-255, higher number is brighter. 
+#define BRIGHTNESS 200  // 0-255, higher number is brighter. 
 #define SATURATION 255   // 0-255, 0 is pure white, 255 is fully saturated color
-#define PALETTE_SPEED  30   // How fast the palette colors move.  Higher numbers = faster motion
 #define STEPS        2   // How wide the bands of color are.  1 = more like a gradient, 10 = more like stripes
 #define BUTTON_PIN   3   // button is connected to pin 2 and GND
-
 #define COLOR_ORDER GRB  // Try mixing up the letters (RGB, GBR, BRG, etc) for a whole new world of color combinations
-#define FIRE_SPEED  15   // Fire Speed; delay in millseconds. Higher delay = slower movement.
 #define LOOPSTART 0
 
+#define LEDMODE_SELECT_DEFAULT_INTERVAL 50  // default scheduling time for LEDMODESELECT
+#define PALETTE_SPEED  30   // How fast the palette colors move.   Higher delay = slower movement.
+#define FIRE_SPEED  85   // Fire Speed; delay in millseconds. Higher delay = slower movement.
 #define CYLON_SPEED 25  // Cylon Speed; delay in millseconds. Higher delay = slower movement.
+#define FADEGLITTER_SPEED 10  // Cylon Speed; delay in millseconds. Higher delay = slower movement.
+#define DISCOGLITTER_SPEED 20  // Cylon Speed; delay in millseconds. Higher delay = slower movement.
 
 CRGB leds[NUM_LEDS];
-CRGBPalette16 currentPalette;
-CRGBPalette16 palettes[] = { RainbowColors_p, RainbowStripeColors_p, OceanColors_p, HeatColors_p, PartyColors_p, CloudColors_p, ForestColors_p } ;
-TBlendType    currentBlending;
 
-int ledMode = 8 ; // Which mode do we start with
+int ledMode = 17 ; // Which mode do we start with
 
-long loopCounter = LOOPSTART ;  // loopCounter. (mis)used in some modes
 
 unsigned long lastButtonChange = 0; // button debounce timer.
 byte currKeyState = LOW ;
 byte prevKeyState = HIGH;         // button is active low
 
-char *routines[] = { "rb", "rb_stripe", "ocean", "heat", "party", "cloud", "forest", "fire2012", "cylon", "cylonmulti", "fglitter", "dglitter", "strobe", "pulse", "pulsestatic", "pulse2", "pulsesuck", "racers", "black" };
+char *routines[] = {
+  "rb",         // 0
+  "rb_stripe",  // 1
+  "ocean",      // 2
+  "heat",       // 3
+  "party",      // 4
+  "cloud",      // 5
+  "forest",     // 6
+  "fire2012",   // 7
+  "cylon",      // 8
+  "cylonmulti", // 9
+  "fglitter",   // 10
+  "dglitter",   // 11
+  "strobe",     // 12
+  "flashbpm",   // 13
+  "pulse",      // 14
+  "pulsestatic",// 15
+  "pulse2",     // 16
+  "pulsesuck",  // 17
+  "racers",     // 18
+  "black"       // 19
+};
 #define NUMROUTINES (sizeof(routines)/sizeof(char *)) //array size  
+
+/* Scheduler stuff */
+void ledModeSelect() ; // prototype method
+Scheduler runner;
+Task taskLedModeSelect( LEDMODE_SELECT_DEFAULT_INTERVAL, TASK_FOREVER, &ledModeSelect); // routine which adds/removes tasks according to ledmode
+#define _TASK_SLEEP_ON_IDLE_RUN
 
 void setup() {
   delay( 1000 ); // power-up safety delay
   FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
-  currentBlending = LINEARBLEND;
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), shortKeyPress, RISING);
 
-  Serial.begin(9600) ;
-  Serial.print( "Starting up. Numroutines = ") ;
-  Serial.println( NUMROUTINES ) ;
+  Serial.begin(115200) ;
+  DEBUG_PRINT( "Starting up. Numroutines = ") ;
+  DEBUG_PRINTLN( NUMROUTINES ) ;
+
+  /* Start the scheduler */
+  runner.init();
+  runner.addTask(taskLedModeSelect);
+  taskLedModeSelect.enable() ;
 }
 
 void loop() {
-  static uint8_t startIndex = 0;  // initialize at start
-  startIndex = startIndex + 1;
-  static uint8_t strobeCount = 10 ;
+  runner.execute();
+}
+
+
+void ledModeSelect() {
+  static long loopCounter = LOOPSTART ;  // loopCounter. (mis)used in pulse
 
   if ( ledMode >= 0 and ledMode <= 6 ) {
-    currentPalette = palettes[ledMode] ;
-    FillLEDsFromPaletteColors( startIndex);
-    FastLED.show();
-    FastLED.delay(1000 / PALETTE_SPEED );
+    FillLEDsFromPaletteColors() ;
+    taskLedModeSelect.setInterval( PALETTE_SPEED ) ;
 
     // FastLED Fire2012 split down the middle, so the fire flows "down" from the neck of the scarf to the ends
   } else if ( strcmp(routines[ledMode], "fire2012") == 0 ) {
     Fire2012() ;
-    FastLED.show();
-    FastLED.delay(1000 / FIRE_SPEED );
+    taskLedModeSelect.setInterval( FIRE_SPEED ) ;
 
     // Cylon / KITT / Larson scanner with fading tail and slowly changing color
   } else if ( strcmp(routines[ledMode], "cylon") == 0 ) {
+    taskLedModeSelect.setInterval( CYLON_SPEED ) ;
     cylon() ;
-    FastLED.show();
-    fadeall(240);
-    delay( CYLON_SPEED );
 
-    // Cylon / KITT / Larson scanner with fading tail and slowly changing color
+    // Cylon / KITT / Larson scanner with 4 "movers"
   } else if ( strcmp(routines[ledMode], "cylonmulti") == 0 ) {
+    taskLedModeSelect.setInterval( CYLON_SPEED ) ;
     cylonMulti() ;
-    FastLED.show();
-    fadeall(200);
-    delay( CYLON_SPEED );
 
     // Fade glitter
   } else if ( strcmp(routines[ledMode], "fglitter") == 0 ) {
-    fadeall(250);
-    addGlitter(90);
-    FastLED.show();
-    FastLED.delay(2);
+    taskLedModeSelect.setInterval( FADEGLITTER_SPEED ) ;
+    fadeGlitter() ;
 
     //  Disco glitter
   } else if ( strcmp(routines[ledMode], "dglitter") == 0 ) {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    addGlitter(90);
-    FastLED.show();
-    FastLED.delay(20);
+    taskLedModeSelect.setInterval( DISCOGLITTER_SPEED ) ;
+    discoGlitter() ;
 
     // With thanks to Hans for the strobe idea https://www.tweaking4all.nl/hardware/arduino/adruino-led-strip-effecten/#strobe
   } else if ( strcmp(routines[ledMode], "strobe") == 0 ) {
-    fill_solid(leds, NUM_LEDS, CRGB::White);  // Pretty awful
-    //    fill_solid(leds, NUM_LEDS, CHSV( random8(), 255, 255));  // A lot worse. Just terrible!
-    FastLED.show();
-    FastLED.delay(40);
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    FastLED.show();
-    FastLED.delay(40);
-
-    // Abuse the palette startIndex counter to pause 1 - 3.5 seconds every strobeCount flashes.
-    if ( (startIndex % strobeCount) == 0 ) {
-      FastLED.delay(random16(1000, 3500));
-      strobeCount = random8(2, 10) ;
-    }
+    //    setInterval is done in the subroutine itself
+    strobe( 0, 10 ) ;
 
     // Black - off
   } else if ( strcmp(routines[ledMode], "black") == 0 ) {
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     FastLED.show();
-    FastLED.delay(500);
+    taskLedModeSelect.setInterval( 500 ) ;  // long because nothing is going on anyways.
+
 
   } else if ( strcmp(routines[ledMode], "pulse") == 0 ) {
     loopCounter++ ;
@@ -133,7 +157,7 @@ void loop() {
     //    Serial.println(":  ") ;
     pulse( loopCounter, loopCounter + 20, 0 ) ;
     FastLED.show();
-    FastLED.delay(20);
+    taskLedModeSelect.setInterval( 20 ) ;
 
   } else if ( strcmp(routines[ledMode], "pulsestatic") == 0 ) {
     pulse_static() ;
@@ -145,23 +169,25 @@ void loop() {
   } else if ( strcmp(routines[ledMode], "pulsesuck") == 0 ) {
     pulse_suck() ;
 
+  } else if ( strcmp(routines[ledMode], "flashbpm") == 0 ) {
+    strobe( 130, 2 ) ;
+
   } else if ( strcmp(routines[ledMode], "racers") == 0 ) {
-    racingLeds() ;
+    racingLeds(loopCounter) ;
     loopCounter++ ;
     FastLED.show();
-    FastLED.delay(5);
+    taskLedModeSelect.setInterval( 5 ) ;
   }
-
 }
 
 // interrupt triggered button press with a very simple debounce (discard multiple button presses < 500ms)
 void shortKeyPress() {
-  if ( millis() - lastButtonChange > 500 ) {
+  if ( millis() - lastButtonChange > 300 ) {
     ledMode++;
-    Serial.print("ledMode = ") ;
-    Serial.print( routines[ledMode] ) ;
-    Serial.print( " mode " ) ;
-    Serial.println( ledMode ) ;
+    DEBUG_PRINT("ledMode = ") ;
+    DEBUG_PRINT( routines[ledMode] ) ;
+    DEBUG_PRINT( " mode " ) ;
+    DEBUG_PRINTLN( ledMode ) ;
 
     if (ledMode >= NUMROUTINES ) {
       ledMode = 0;
@@ -169,17 +195,24 @@ void shortKeyPress() {
 
     lastButtonChange = millis() ;
   } else {
-    // Debugging output
-    // Serial.println( "Too short an interval" ) ;
+    //    DEBUG_PRINTLN( "Too short an interval" ) ;
   }
 }
 
-void FillLEDsFromPaletteColors( uint8_t colorIndex) {
+void FillLEDsFromPaletteColors() {
+  const CRGBPalette16 palettes[] = { RainbowColors_p, RainbowStripeColors_p, OceanColors_p, HeatColors_p, PartyColors_p, CloudColors_p, ForestColors_p } ;
+  static uint8_t startIndex = 0;  // initialize at start
+  startIndex++;
+
+  uint8_t colorIndex = startIndex ;
+
   for ( int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = ColorFromPalette( currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+    leds[i] = ColorFromPalette( palettes[ledMode], colorIndex, BRIGHTNESS, LINEARBLEND);
     colorIndex += STEPS;
   }
   addGlitter(80);
+
+  FastLED.show();
 }
 
 void addGlitter( fract8 chanceOfGlitter)
@@ -197,22 +230,36 @@ void addColorGlitter( fract8 chanceOfGlitter)
   }
 }
 
+void fadeGlitter() {
+  addGlitter(90);
+  FastLED.show();
+  fadeall(250);
+}
 
+void discoGlitter() {
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  addGlitter(90);
+  FastLED.show();
+}
+
+
+// If you want to restrict the color cycling to a HSV range, adjust these:
 #define STARTHUE 0
 #define ENDHUE 255
 
 void cylon() {
-  static uint8_t hueAdder = 1 ;
+  static uint8_t hueAdder = 1 ;  // set to higher for faster color cycling
   static uint8_t hue = STARTHUE;
   static uint8_t ledPosAdder = 1 ;
   static uint8_t ledPos = 0;
 
   leds[ledPos] = CHSV(hue, 255, 255);
 
-  // If current LED position is divisible by 5, increase hue by 1
+  // If current LED position is divisible by 5, increase hue by hueAdder
   if ( ledPos % 5 == 0 ) {
     hue += hueAdder ;
   }
+  // if we reach ENDHUE or STARTHUE, start cycling back.
   if ( hue > ENDHUE or hue == STARTHUE ) {
     hueAdder *= -1 ;
   }
@@ -223,10 +270,13 @@ void cylon() {
     ledPos += ledPosAdder ;
   }
 
+  FastLED.show();
+  fadeall(230);
+
 }
 
 void cylonMulti() {
-  static uint8_t ledPos[] = {0, 25, 50, 75}; // Starting position
+  static uint8_t ledPos[] = {0, 29, 48, 87}; // Starting position
   static int ledAdd[] = {1, 1, 1, 1}; // Starting direction
 
   for (int i = 0; i < 4; i++) {
@@ -236,6 +286,9 @@ void cylonMulti() {
     }
     ledPos[i] += ledAdd[i] ;
   }
+
+  FastLED.show();
+  fadeall(180);
 }
 
 
@@ -251,6 +304,37 @@ void brightall(uint8_t bright_all_speed) {
   }
 }
 
+#define STROBE_ON_TIME 40 
+
+// Pretty awful - current timings are like lightning
+void strobe( int bpm, uint8_t numStrobes ) {
+  static uint8_t strobesToDo = numStrobes ;
+  
+  taskLedModeSelect.setInterval(STROBE_ON_TIME); // run this task every STROBE_ON_TIME seconds
+
+  //  DEBUG_PRINTLN( taskLedModeSelect.getRunCounter() ) ;
+
+  if ( (taskLedModeSelect.getRunCounter() % 2 ) == 0 ) {
+    fill_solid(leds, NUM_LEDS, CRGB::White);
+  } else {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+  }
+  FastLED.show();
+
+  // use getRunCounter (number of iterations of taskLedModeSelect), and if evenly divisible by strobesToDo, wait a bit
+  if ( (taskLedModeSelect.getRunCounter() % strobesToDo) == 0 ) {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    if ( bpm != 0 ) {
+      // If we want to numStrobes of STROBE_ON_TIME and numStrobes of black we need to subtract it from the BPM to delay calculation
+      taskLedModeSelect.setInterval( round(60000 / bpm) - ( STROBE_ON_TIME * numStrobes * 2) );
+    } else {
+      // Lightning simulation
+      strobesToDo = random8(4, 12) ;
+      taskLedModeSelect.setInterval(random16(1000, 2500));
+    }
+  }
+}
 
 void pulse(uint8_t startPixelPos, uint8_t endPixelPos, uint8_t hue ) {
   uint8_t middlePixelPos = endPixelPos - round( (endPixelPos - startPixelPos) / 2 ) ;
@@ -294,7 +378,7 @@ void pulse2() {
   static bool flowDir = 1; // remember flowDir between calls to pulse2
 
   fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.delay(random16(100, 1000)) ;
+//  taskLedModeSelect.delay(random16(100, 1000)) ;
 
   hue = random8(0, 60) ;
   brightness = 1 ;
@@ -339,7 +423,7 @@ void pulse2() {
     fill_gradient(leds, startP, CHSV(hue, 255, 0), middle, CHSV(hue, 255, brightness), SHORTEST_HUES);
     fill_gradient(leds, middle, CHSV(hue, 255, brightness), endP, CHSV(hue, 255, 0), SHORTEST_HUES);
     FastLED.show();
-    FastLED.delay(25);
+//    taskLedModeSelect.delay(25);
   }
 }
 
@@ -347,30 +431,26 @@ void pulse2() {
 
 void pulse_static() {
   int middle ;
-  int startP ;
-  int endP ;
-  uint8_t hue ;
-  int brightness;
-  int bAdder ;
-  // bool flowDir ;
+  static int startP ;
+  static int endP ;
+  static uint8_t hue ;
+  static int bAdder ;
 
-  //  while ( ledMode == 8  ) {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.delay(random16(200, 700)) ;
+  static int brightness = 0 ;
+  static bool sequenceEnd ;
 
-  hue = random8() ;
-  brightness = MIN_BRIGHT + 1 ;
-  bAdder = 10 ;
-  startP = random8(1, 70);
-  endP = startP + 30 ;
+  if ( brightness < MIN_BRIGHT ) {
+    sequenceEnd = true ;
+  }
 
-
-  while ( brightness > MIN_BRIGHT ) {
+  // while brightness is more than MIN_BRIGHT, keep increasing brightness etc.
+  // If brightness drops below MIN_BRIGHT, we start a new sequence at a new position
+  if ( not sequenceEnd ) {
     if ( bAdder < 0 and startP < endP ) {
       startP++ ;
       endP-- ;
       if ( startP == endP ) {
-        break ;
+        sequenceEnd = true ;
       }
     }
     if ( bAdder > 0  and ( endP - startP < 30 ) ) {
@@ -387,18 +467,26 @@ void pulse_static() {
     brightness = constrain(brightness, 0, 255) ;
     if ( brightness >= 250 ) {
       bAdder = -5 ;
-      //        Serial.print(" bAdder: ") ;
-      //        Serial.print(bAdder) ;
-      //        brightness += bAdder ;
     }
 
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     fill_gradient(leds, startP, CHSV(hue, 255, 0), middle, CHSV(hue, 255, brightness), SHORTEST_HUES);
     fill_gradient(leds, middle, CHSV(hue, 255, brightness), endP, CHSV(hue, 255, 0), SHORTEST_HUES);
     FastLED.show();
-    FastLED.delay(25);
+    taskLedModeSelect.setInterval( 25 ) ;
   }
-  // }
+
+  if ( sequenceEnd ) {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    hue = random8(0, 60) ;
+    brightness = MIN_BRIGHT + 1 ;
+    bAdder = 10 ;
+    startP = random8(1, 70);
+    endP = startP + 30 ;
+    sequenceEnd = false ;
+    taskLedModeSelect.setInterval(random16(200, 700)) ;
+  }
 }
 
 void pulse_suck() {
@@ -411,7 +499,7 @@ void pulse_suck() {
   static int lastPixelEndPos ;
 
   fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.delay(random16(200, 700)) ;
+  taskLedModeSelect.delay(random16(200, 700)) ;
 
   hue = random8(0, 60) ;
   brightness = MIN_BRIGHT + 1 ;
@@ -449,7 +537,7 @@ void pulse_suck() {
     fill_gradient(leds, startPixelPos, CHSV(hue, 255, 0), middle, CHSV(hue, 255, brightness), SHORTEST_HUES);
     fill_gradient(leds, middle, CHSV(hue, 255, brightness), endPixelPos, CHSV(hue, 255, 0), SHORTEST_HUES);
     FastLED.show();
-    FastLED.delay(25);
+    taskLedModeSelect.delay(25);
   }
 }
 
@@ -504,9 +592,10 @@ void Fire2012()
     leds[ledIndex] = color;
   }
 
+  FastLED.show();
 }
 
-void racingLeds() {
+void racingLeds(long loopCounter ) {
   static uint8_t racer[] = {0, 1, 2}; // Starting positions
   static int racerDir[] = {1, 1, 1}; // Current direction
   static int racerSpeed[] = { random8(1, 4), random8(1, 4) , random8(1, 4) }; // Starting speed
@@ -519,7 +608,7 @@ void racingLeds() {
   for ( int i = 0; i < NUMRACERS ; i++ ) {
     leds[racer[i]] = racerColor[i]; // Assign color
 
-    // If the global variable "loopcounter" is evenly divisible by 'speed' then check if we've reached the end (if so, reverse), and do a step
+    // If "loopcounter" is evenly divisible by 'speed' then check if we've reached the end (if so, reverse), and do a step
     if ( loopCounter % racerSpeed[i] == 0 ) {
       if ( (racer[i] + racerDir[i] >= NUM_LEDS) or (racer[i] + racerDir[i] <= 0) ) {
         racerDir[i] *= -1 ;
